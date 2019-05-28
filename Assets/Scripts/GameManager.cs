295ab3be79a546daade311d16e249d258bdfd85b
Noah Ratcliff <me@nratcliff.com>
2019-05-28 17:08:03 -0400

    paddle follows mouse

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance { get; private set; }
+
+	public Camera MainCamera;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Debug.LogError("[GameManager] More than one instance of " +
+				"GameManager in the scene! Destroying self...");
+			DestroyImmediate(this);
+			return;
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+	private GameManager manager => GameManager.Instance;
+
+	private void Update()
+	{
+		FollowMouse();
+	}
+
+	/// <summary>
+	/// Make our position the same as the mouse X position in world coordinates
+	/// </summary>
+	private void FollowMouse()
+	{
+		// get mouse pos in world space
+		Vector2 mousePos = Input.mousePosition;
+		Vector2 worldMousePos = manager.MainCamera.ScreenToWorldPoint(mousePos);
+
+		// set our x position to mouse x
+		Vector3 position = transform.position;
+		position.x = worldMousePos.x;
+		transform.position = position;
+	}
+}
