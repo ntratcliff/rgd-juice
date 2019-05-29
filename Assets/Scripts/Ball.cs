1b9935d006e78fd2f96f81dce3b0bc496c65a651
Noah Ratcliff <me@nratcliff.com>
2019-05-29 18:45:21 -0400

    finish demo :)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,12 +6,27 @@ public class Ball : MonoBehaviour
 	public float Size = 0.3f;
 	public float Speed = 1f;
 
+	[Range(1f, 10f)]
+	public float SquishIntensity;
+
+	[Range(0.01f, 1f)]
+	public float SquishReturnSpeed;
+
+	[Range(0f, 1f)]
+	public float StretchIntensity;
+
+	public float PitchRange;
+
 	public SpriteRenderer SpriteRenderer;
 	public CircleCollider2D Collider;
+	public ParticleSystem ParticleSystem;
 
 	public Rigidbody2D Body { get; private set; }
 
-	private Vector2 velocity;
+	public Vector2 Velocity { get; private set; }
+
+	private GameManager manager => GameManager.Instance;
+	private AudioSource audioSource;
 
 	private void Awake()
 	{
@@ -22,12 +37,25 @@ public class Ball : MonoBehaviour
 		// using a rigidbody just to get collision events becuase I'm lazy
 		Body = GetComponent<Rigidbody2D>();
 
-		velocity = -Vector2.one * Speed;
+		audioSource = GetComponent<AudioSource>();
+
+		Velocity = -Vector2.one * Speed;
 	}
 
 	private void FixedUpdate()
 	{
-		transform.Translate(velocity * Time.fixedDeltaTime);
+		transform.position += (Vector3)Velocity * Time.fixedDeltaTime;
+	}
+
+	private void Update()
+	{
+		if (manager.RotateBall)
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, Velocity);
+		else
+			transform.rotation = Quaternion.Euler(Vector3.zero);
+
+		// return from squish
+		transform.localScale += (Vector3.one - transform.localScale) * SquishReturnSpeed;
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
@@ -39,17 +67,61 @@ public class Ball : MonoBehaviour
 			block.Destroy(this);
 		}
 
+		Paddle paddle = collision.collider.GetComponentInParent<Paddle>();
+		if (paddle)
+		{
+			paddle.OnBallHit();
+		}
+
+		manager.OnBallHit(Velocity.normalized);
+
+		// squash and stretch animation
+		Squish();
+
 		// reflect velocity across surface normal
-		velocity = Vector2.Reflect(
-			inDirection: velocity,
+		Velocity = Vector2.Reflect(
+			inDirection: Velocity,
 			inNormal: collision.GetContact(0).normal
 		);
 
 		// surface normals can be a bit unreliable in unity, 
 		// preserve 45 degree direction and speed after reflect
-		velocity = new Vector2(
-			x: Mathf.Sign(velocity.x),
-			y: Mathf.Sign(velocity.y)
+		Velocity = new Vector2(
+			x: Mathf.Sign(Velocity.x),
+			y: Mathf.Sign(Velocity.y)
 		) * Speed;
+
+		// rotate before particles
+		if (manager.RotateBall)
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, Velocity);
+
+		// dust particles
+		if (manager.BallParticles)
+			ParticleSystem.Emit(5);
+
+		// sfx
+		if (manager.BallSFX)
+		{
+			if(manager.RandomizePitch)
+			{
+				audioSource.pitch = 1f + Random.Range(-PitchRange, PitchRange);
+			}
+
+			audioSource.PlayOneShot(audioSource.clip);
+		}
+	}
+
+	private void Squish()
+	{
+		if (!manager.BallSquish) { return; }
+
+		// squash and stretch animation for ball
+		Vector3 scale = Vector3.one;
+
+		// squish along X, inverse along Y to preserve volume
+		scale.x = 1f / SquishIntensity;
+		scale.y = StretchIntensity / scale.x;
+
+		transform.localScale = scale;
 	}
 }
--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Please feel free to take any of these scripts to make your own game juicier!
 
 ### Unity Version: 2018.3.7f1
 
+### Music Credits
+Pookatori and Friends Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 3.0 License
+http://creativecommons.org/licenses/by/3.0/
+
